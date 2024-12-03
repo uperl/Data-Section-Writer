a7f8234fecc5ee6f32aa2ad23dd00d134ad47c28
Graham Ollis <plicease@cpan.org>
2024-12-03 09:23:54 -0700

    start

--- lib/Data/Section/Writer.pm
+++ lib/Data/Section/Writer.pm
@@ -5,7 +5,79 @@ use experimental qw( postderef signatures );
 package Data::Section::Writer {
 
   # ABSTRACT: Write __DATA__ section files for Data::Section, Data::Section::Simple or Mojo::Loader::data_section
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ATTRIBUTES
+
+=head2 perl_filename
+
+=cut
+
+  use Path::Tiny ();
+  use Carp ();
+  use Class::Tiny qw( perl_filename _files );
+  use Ref::Util qw( is_blessed_ref );
+  use MIME::Base64 qw(encode_base64);
+
+  sub BUILD ($self, $) {
+
+    # use the callers filename if not provided.
+    unless(defined $self->perl_filename) {
+      my(undef, $fn) = caller 2;
+      $self->perl_filename($fn);
+    }
+
+    # upgrade to Path::Tiny if it is not already
+    unless(is_blessed_ref $self->perl_filename && $self->isa('Path::Tiny')) {
+      $self->perl_filename(Path::Tiny->new($self->perl_filename));
+    }
+
+    $self->_files({});
+
+  }
+
+=head1 METHODS
+
+=head2 add_file
+
+=cut
+
+  sub add_file ($self, $filename, $content, $encoding=undef) {
+    Carp::croak("Unknown encoding $encoding") if defined $encoding && $encoding ne 'base64';
+    $self->_files->{"$filename"} = [ $content, $encoding ];
+    $self;
+  }
+
+  sub _render_file ($self, $filename, $data) {
+    my $text = "@@ $filename";
+    $text .= " (" . $data->[1] . ")" if defined $data->[1];
+    $text .= "\n";
+    $DB::single = 1;
+    if(defined $data->[1] && $data->[1] eq 'base64') {
+      $text .= encode_base64($data->[0]);
+    } else {
+      $text .= $data->[0];
+    }
+    return $text;
+  }
+
+=head2 render_section
+
+=cut
+
+  sub render_section ($self) {
+    my $files = $self->_files;
+    return "__DATA__\n" unless %$files;
+    return join("\n",
+      "__DATA__",
+      "",
+      (map { $self->_render_file($_, $files->{$_}) } sort keys $files->%*),
+      ''
+    );
+  }
 }
 
 1;
-
--- t/data_section_writer.t
+++ t/data_section_writer.t
@@ -1,7 +1,50 @@
 use Test2::V0 -no_srand => 1;
 use Data::Section::Writer;
 
-ok 1, 'todo';
+is(
+  Data::Section::Writer->new,
+  object {
+    prop isa => 'Data::Section::Writer';
+    call perl_filename => object {
+      prop isa => 'Path::Tiny';
+      call stringify => __FILE__;
+    };
+    call render_section => "__DATA__\n";
+  },
+  'defaults',
+);
+
+is(
+  Data::Section::Writer->new( perl_filename => 'Foo/Bar.pm' ),
+  object {
+    prop isa => 'Data::Section::Writer';
+    call perl_filename => object {
+      prop isa => 'Path::Tiny';
+      call stringify => 'Foo/Bar.pm';
+    };
+    call render_section => "__DATA__\n";
+  },
+  'upgrade perl_filename',
+);
+
+is(
+  Data::Section::Writer->new,
+  object {
+    prop isa => 'Data::Section::Writer';
+    call [add_file => 'foo.txt', "Foo Bar Baz"] => object {
+      prop isa => 'Data::Section::Writer';
+    };
+    call [add_file => 'bar.bin', "Foo Bar Baz", 'base64'] => object {
+      prop isa => 'Data::Section::Writer';
+    };
+    call render_section => "__DATA__\n\n" .
+                           "\@\@ bar.bin (base64)\n" .
+                           "Rm9vIEJhciBCYXo=\n\n" .
+                           "\@\@ foo.txt\n" .
+                           "Foo Bar Baz\n";
+  },
+  'add_section_file',
+);
 
 done_testing;
 
