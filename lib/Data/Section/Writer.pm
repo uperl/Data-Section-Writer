ae1f650822aaec96fcc0e0bd91185175f6b9ecf6
Graham Ollis <plicease@cpan.org>
2024-12-03 08:38:29 -0700

    Initial structure

---
+++ lib/Data/Section/Writer.pm
@@ -0,0 +1,11 @@
+use warnings;
+use 5.020;
+use experimental qw( postderef signatures );
+
+package Data::Section::Writer {
+
+  # ABSTRACT: Write __DATA__ section files for Data::Section, Data::Section::Simple or Mojo::Loader::data_section
+}
+
+1;
+
---
+++ t/data_section_writer.t
@@ -0,0 +1,8 @@
+use Test2::V0 -no_srand => 1;
+use Data::Section::Writer;
+
+ok 1, 'todo';
+
+done_testing;
+
+
